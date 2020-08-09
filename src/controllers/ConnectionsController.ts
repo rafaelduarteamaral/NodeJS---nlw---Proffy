acb3dcb1ed251b7d928a236b8baed6f90b6d2c90
Rafael Duarte Amaral <rafael.duarte.amaral@gmail.com>
2020-08-09 08:59:08 -0300

    first commit

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response, request, response } from 'express';
+import db from './../database/connection';
+import convertHourToMinutes from './../Utils/convertHourToMinutes';
+
+class ConnectionsController {
+  async index(req: Request, res: Response) {
+    const totalConnections = await db('connections').count('* as total');
+    const { total } = totalConnections[0];
+    return res.json({ total });
+  }
+
+  async create(req: Request, res: Response) {
+
+    const { user_id } = req.body;
+
+    await db('connections').insert({
+      user_id,
+    });
+
+    return res.status(201).send();
+  }
+}
+
+export default new ConnectionsController;
